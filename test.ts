38c8a5f8fea9c4a24a3bb42f5a68d9c9123543aa
shaoziyang <shaoziyang@126.com>
2023-01-26 13:04:03 +0800

    first version

---
+++ test.ts
@@ -0,0 +1,12 @@
+let pressure = 0
+let humidity = 0
+let temperature = 0
+basic.forever(function () {
+    pressure = BME280.pressure(BME280_P.Pa)
+    humidity = BME280.humidity()
+    temperature = BME280.temperature(BME280_T.T_C)
+    serial.writeValue("P", pressure)
+    serial.writeValue("H", humidity)
+    serial.writeValue("T", temperature)
+    basic.pause(1000)
+})
